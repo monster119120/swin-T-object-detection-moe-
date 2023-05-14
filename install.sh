cdcfcb2b068dbb1e9f08dd7ef4a735836ae96cbb
azuna <32482525+monster119120@users.noreply.github.com>
2023-05-14 13:56:56 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+conda create -n swin python==3.8
+conda activate swin
+
+
+pip install torch==1.10.0+cu111 torchvision==0.11.0+cu111 torchaudio==0.10.0 -f https://download.pytorch.org/whl/torch_stable.html
+pip install mmcv-full==1.3.16 -f https://download.openmmlab.com/mmcv/dist/cu111/torch1.10.0/index.html
+
+
+git clone https://github.com/monster119120/swin-T-object-detection-moe-.git
+cd swin-T-object-detection-moe-
+pip install -e .
+
+git clone https://github.com/SJTU1037/tutel.git
+cd tutel
+pip install -e .
+cd ..
+
+
+pip install pycocotools
+pip uninstall pycocotools
+pip install mmpycocotools
+
+pip install numpy==1.23.5
